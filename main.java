d140ca80b69a2e86cee8c57213066cd76eb3e249
Mario Peña <65502311+Mariopolonia0@users.noreply.github.com>
2022-04-06 17:22:50 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+/*
+Mario Peña Polonia 2016-0037
+Proyecto Para Programacion 3
+Edicion 4.0
+*/
+
+public class main{
+	
+	public static void main(String[] args) {
+		Inicio inicio= new Inicio();
+    }
+}
